ff562219832c2ca3f005e214986391772ee12c90
rivercat <h90093000054@gmail.com>
2023-10-23 00:01:47 +0800

    Init files
    Initialize all files

---
+++ graduation_project_demo_backend/src/main/java/com/example/graduation_project_demo_backend/GraduationProjectDemoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.graduation_project_demo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationProjectDemoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationProjectDemoBackendApplication.class, args);
+	}
+
+}
---
+++ graduation_project_demo_backend/src/test/java/com/example/graduation_project_demo_backend/GraduationProjectDemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graduation_project_demo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationProjectDemoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
