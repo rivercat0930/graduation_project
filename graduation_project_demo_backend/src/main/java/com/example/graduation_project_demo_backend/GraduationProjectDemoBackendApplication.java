14ef12c9433cf3b443d8ce05eafdfa144648ed1a
rivercat <h90093000054@gmail.com>
2023-11-08 21:07:09 +0800

    Fix bug
    fix no config problem and can not find h2 driver

--- graduation_project_demo_backend/src/main/java/com/example/graduation_project_demo_backend/GraduationProjectDemoBackendApplication.java
+++ graduation_project_demo_backend/src/main/java/com/example/graduation_project_demo_backend/GraduationProjectDemoBackendApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.example.graduation_project_demo_backend","com.example.graduation_project_demo_backend.model","com.example.graduation_project_demo_backend.controller","com.example.graduation_project_demo_backend.service"})
-@EntityScan({"com.example.graduation_project_demo_backend","com.example.graduation_project_demo_backend.model","com.example.graduation_project_demo_backend.controller","com.example.graduation_project_demo_backend.service"})
 public class GraduationProjectDemoBackendApplication {
 
 	public static void main(String[] args) {
