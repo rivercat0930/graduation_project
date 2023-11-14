16a829587851547afccbdf86862604c5c58d195f
Natsusaka <zz5120937@gmail.com>
2023-11-14 17:33:39 +0800

    feat/add service been, new /test controller

--- graduation_project_demo_backend/src/main/java/com/example/graduation_project_demo_backend/controller/SentimentAnalysisController.java
+++ graduation_project_demo_backend/src/main/java/com/example/graduation_project_demo_backend/controller/SentimentAnalysisController.java
@@ -2,15 +2,18 @@ package com.example.graduation_project_demo_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.graduation_project_demo_backend.service.SentimentAnaysisService;
 
 import java.io.IOException;
 
-@RestController("/sentimentAnalysis")
+@RestController
 @CrossOrigin("*")
+@RequestMapping("/sentimentAnalysis")
 public class SentimentAnalysisController {
   @Autowired
   private SentimentAnaysisService sentimentAnalysisService;
@@ -22,6 +25,15 @@ public class SentimentAnalysisController {
       return "write success";
     } catch (Exception e) {
       return "write false:" + e.getMessage();
+    }
+  }
+
+  @GetMapping("/test")
+  public String test() throws Exception, InterruptedException {
+    try {
+      return "write success";
+    } catch (Exception e) {
+      return "write false:" ;
     }
   }
 }
--- graduation_project_demo_backend/src/main/java/com/example/graduation_project_demo_backend/service/SentimentAnaysisService.java
+++ graduation_project_demo_backend/src/main/java/com/example/graduation_project_demo_backend/service/SentimentAnaysisService.java
@@ -2,11 +2,15 @@ package com.example.graduation_project_demo_backend.service;
 
 import java.io.*;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SentimentAnaysisService {
 	public boolean writeToFile() {
 		try {
-			String command = "python /Users/natsusaka/Desktop/cemotion_test/main.py ";
+			String command = "python3 /Users/natsusaka/Desktop/graduation_project/graduation_project_demo_backend/for_recommend/user_location_reviews.py";
 			Process process = Runtime.getRuntime().exec(command);
+			System.out.println("Successssssss");
 			int exitCode = process.waitFor();
 
 			return exitCode == 0;
