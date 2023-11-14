9e3d6d819aaed8e6e7f72dad2718182ba66d7a34
Natsusaka <zz5120937@gmail.com>
2023-11-14 17:34:06 +0800

    feat/update

--- graduation_project_demo_backend/src/main/java/com/example/graduation_project_demo_backend/GraduationProjectDemoBackendApplication.java
+++ graduation_project_demo_backend/src/main/java/com/example/graduation_project_demo_backend/GraduationProjectDemoBackendApplication.java
@@ -2,8 +2,6 @@ package com.example.graduation_project_demo_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class GraduationProjectDemoBackendApplication {
