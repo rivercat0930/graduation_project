22dbc8959c5020ba7aa28a82cff4dd295bffcb71
Natsusaka <zz5120937@gmail.com>
2023-11-15 13:52:40 +0800

    fix/run python file bug

--- graduation_project_demo_backend/src/main/java/com/example/graduation_project_demo_backend/service/SentimentAnaysisService.java
+++ graduation_project_demo_backend/src/main/java/com/example/graduation_project_demo_backend/service/SentimentAnaysisService.java
@@ -1,6 +1,8 @@
 package com.example.graduation_project_demo_backend.service;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.springframework.stereotype.Service;
 
@@ -8,13 +10,26 @@ import org.springframework.stereotype.Service;
 public class SentimentAnaysisService {
 	public boolean writeToFile() {
 		try {
-			String command = "python3 /Users/natsusaka/Desktop/graduation_project/graduation_project_demo_backend/for_recommend/user_location_reviews.py";
-			Process process = Runtime.getRuntime().exec(command);
-			System.out.println("Successssssss");
-			int exitCode = process.waitFor();
+			// String command = "python3 ./for_recommend/test.py";
+			// String command = "python3 ./for_recommend/user_location_reviews.py;
+			String command[] = { "/usr/bin/python3",
+					"/Users/natsusaka/Desktop/graduation_project/graduation_project_demo_backend/for_recommend/user_location_reviews.py" };
 
+			ProcessBuilder processBuilder = new ProcessBuilder(command);
+			processBuilder.redirectErrorStream(true); // 將錯誤流合併到輸出流
+
+			Process process = processBuilder.start();
+			BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+			String line;
+			while ((line = input.readLine()) != null) {
+				System.out.println("===================\n" + line);
+			}
+
+			int exitCode = process.waitFor();
 			return exitCode == 0;
 		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
 			return false;
 		}
 	}
