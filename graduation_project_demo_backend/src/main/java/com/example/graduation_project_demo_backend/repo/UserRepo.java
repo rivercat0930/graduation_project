48ec97fb6e5c01964b064b8ecff5bbf5b5b41e05
Natsusaka <zz5120937@gmail.com>
2023-11-16 13:22:35 +0800

    feat/update

--- graduation_project_demo_backend/src/main/java/com/example/graduation_project_demo_backend/repo/UserRepo.java
+++ graduation_project_demo_backend/src/main/java/com/example/graduation_project_demo_backend/repo/UserRepo.java
@@ -2,6 +2,7 @@ package com.example.graduation_project_demo_backend.repo;
 
 import com.example.graduation_project_demo_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface UserRepo extends JpaRepository<User, Integer> {
   // void deleteUserByUid(Integer id);
